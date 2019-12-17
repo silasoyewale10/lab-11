6fc494cbfa3af16ae195cd524ce3390abf6b0d9b
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-12-17 11:48:26 -0800

    front end error

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const ejs = require('ejs');
-// const superagent = require('superagent');
+const superagent = require('superagent');
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3050;
 
 app.use(express.static('./public'));
 // new middleware is urlencoded
@@ -16,7 +16,34 @@ app.get('/', (req, res) => {
     res.render('pages/index');
 })
 
+// app.post('/', (req, res) => {
+//     superagent.get(`https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author}`).then(data => {
+//       const books = data.body.items.map(book => ({name: book.volumeInfo.title}));
+//       console.log(books);
+//       res.render('book-results', {
+//         books: books
+//       });
+//     });
+//   });
 
+function Book(query, response) {
+    // this.image = image,
+    // this.title = title,
+    // this.author = author,
+    // this.description = description
+}
+
+app.post('/', (req, res) => {
+    const url = `https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author}`;
+    superagent.get(url).then(data => {
+        let query = req.body.author;
+        console.log('1111', query);
+        let narrowedData = data.body.items[0].volumeInfo;
+        const newBook = new Book(query, narrowedData);
+        // console.log(data.body.items[0].volumeInfo);
+        // const books = data.body.items
+    })
+})
 
 
 
