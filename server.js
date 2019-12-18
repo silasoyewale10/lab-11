98069e6272d6cb9e17afa4f58c0b1520adb9f6a9
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-12-17 23:01:50 -0800

    error fixed

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const ejs = require('ejs');
 const superagent = require('superagent');
 const app = express();
-const PORT = process.env.PORT || 3060;
+const PORT = process.env.PORT || 3050;
 app.use(express.static('./public'));
 // new middleware is urlencoded
 app.use(express.urlencoded( {extended:true} ));
 app.set('view engine', 'ejs');
 app.set('views', './views/pages');
+
+
 app.get('/', (req, res) => {
     res.render('index');
 })
@@ -28,7 +30,7 @@ function Book(url, title, author, description) {
 }
 app.post('/', (req, res) => {
     // console.log('22222', req.body.authorText);
-    // res.render('searches');
+    // res.render('show');
     const url = `https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.authorText}`;
     superagent.get(url).then(data => {
         const books =[];
@@ -38,8 +40,13 @@ app.post('/', (req, res) => {
             // console.log(element.volumeInfo.imageLinks.thumbnail);
             books.push(new Book(element.volumeInfo.imageLinks.thumbnail, element.volumeInfo.title, element.volumeInfo.authors, element.volumeInfo.description));
         })
-        res.render('searches', {books: books});
-    })
+        res.render('searches/show', {books: books});
+    }).catch(err => errorHandler(err, res));
 })
+
+function errorHandler(error, response) {
+    console.error(error);
+    response.render('Ooops! Something went wrong');
+}
 // app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
