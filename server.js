8f947dd697e2a2d22077983741237bffbb2b0903
Silas Oyewale <54855028+silasoyewale10@users.noreply.github.com>
2019-12-17 22:49:57 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,15 @@ const express = require('express');
 const ejs = require('ejs');
 const superagent = require('superagent');
 const app = express();
-const PORT = process.env.PORT || 3020;
-
+const PORT = process.env.PORT || 3060;
 app.use(express.static('./public'));
 // new middleware is urlencoded
 app.use(express.urlencoded( {extended:true} ));
-
-
 app.set('view engine', 'ejs');
-
-
+app.set('views', './views/pages');
 app.get('/', (req, res) => {
-    res.render('pages/index');
+    res.render('index');
 })
-
 // app.post('/', (req, res) => {
 //     superagent.get(`https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author}`).then(data => {
 //       const books = data.body.items.map(book => ({name: book.volumeInfo.title}));
@@ -25,40 +20,26 @@ app.get('/', (req, res) => {
 //       });
 //     });
 //   });
-
-function Book(query, response) {
-    // this.image = image,
-    // this.title = title,
-    // this.author = author,
-    // this.description = description
+function Book(url, title, author, description) {
+    this.image = url,
+    this.title = title,
+    this.author = author,
+    this.description = description
 }
-
 app.post('/', (req, res) => {
-    console.log('22222', req.body.authorText);
+    // console.log('22222', req.body.authorText);
+    // res.render('searches');
     const url = `https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.authorText}`;
     superagent.get(url).then(data => {
         const books =[];
-        
-        for (let i = 0; i< 10; i++) {
-            books.push(data.body.items[i].volumeInfo);
-            if (i === 9) {
-                return;
-            }
-        }
-        console.log('333333', books);
-        // console.log('1111', data.body.items[i].volumeInfo);
-        let query = req.body;
-        // console.log('1111', query);
-        let narrowedData = data.body.items[0].volumeInfo;
-        const newBook = new Book(query, narrowedData);
-        // console.log(data.body.items[0].volumeInfo);
-        // const books = data.body.items
+        // console.log('1111', data.body.items[0].volumeInfo);
+        let narrowedData = data.body.items;
+        narrowedData.forEach(element => {
+            // console.log(element.volumeInfo.imageLinks.thumbnail);
+            books.push(new Book(element.volumeInfo.imageLinks.thumbnail, element.volumeInfo.title, element.volumeInfo.authors, element.volumeInfo.description));
+        })
+        res.render('searches', {books: books});
     })
 })
-
-
-
-app.get('*', (request, response) => response.status(404).send('This route does not exist'));
-
-
+// app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
