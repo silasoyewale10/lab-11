b19c477cecc8296f3268f1eacea604e578e7424e
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-12-19 00:14:33 -0800

    made database connection

--- books.sql
+++ books.sql
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-CREATE TABLE books (
-  id SERIAL PRIMARY KEY,
-  author VARCHAR(255),
-  title VARCHAR(255),
-  isbn VARCHAR(255),
-  image_url VARCHAR(255),
-  descriptions TEXT,
-  bookshelf VARCHAR(255)
-=======
 
 
 CREATE TABLE IF NOT EXISTS books(
     id SERIAL PRIMARY KEY,
     author VARCHAR(255),
     title VARCHAR(255),
-    isbn INTEGER,
+    isbn NUMERIC(25, 10), 
     image_url VARCHAR(255),
     descriptions TEXT,
     bookshelf VARCHAR(255)
->>>>>>> affb29dd798cdd300273e2da7cc103f2cf1aaac1
 );
