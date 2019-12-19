95e6645f2853528750410efec957956f5b0e7dbc
Silas Oyewale <silasoyewale10@gmail.com>
2019-12-19 10:27:28 -0800

    committing

--- books.sql
+++ books.sql
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-CREATE TABLE books (
-  id SERIAL PRIMARY KEY,
-  author VARCHAR(255),
-  title VARCHAR(255),
-  isbn VARCHAR(255),
-  image_url VARCHAR(255),
-  descriptions TEXT,
-  bookshelf VARCHAR(255)
-=======
-
-
-CREATE TABLE IF NOT EXISTS books(
+CREATE TABLE books(
     id SERIAL PRIMARY KEY,
     author VARCHAR(255),
     title VARCHAR(255),
@@ -18,5 +6,4 @@ CREATE TABLE IF NOT EXISTS books(
     image_url VARCHAR(255),
     descriptions TEXT,
     bookshelf VARCHAR(255)
->>>>>>> affb29dd798cdd300273e2da7cc103f2cf1aaac1
 );
