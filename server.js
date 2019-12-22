7704755f2c45e9ccbc99047cbf01d9ca479ab3fb
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-12-21 21:34:18 -0800

    added feature 4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const superagent = require('superagent');
 const app = express();
 
 const pg = require('pg');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 require('dotenv').config();
 app.use(express.static('./public'));
 // new middleware is urlencoded
@@ -16,77 +16,72 @@ app.get('/', (req, res) => {
     res.render('searches/new');
 });
 
-// app.get('/', (req, res) => {
-//     res.render('index');
-// });
-
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', error => console.log(error));
 client.connect();
 
-function Book(url, title, author, description, isbn = 'Not Available', bookshelf ='none') {
-    (this.image = url), (this.title = title), (this.author = author), (this.description = description), (this.isbn = isbn), (bookshelf = bookshelf);
+function Book(url = 'Not Available', title, author, description, isbn = 'Not Available', bookshelf ='none') {
+    (this.image = url || undefined), (this.title = title), (this.author = author), (this.description = description), (this.isbn = isbn), (bookshelf = bookshelf);
 }
 app.post('/', getBooksData);
 
 function getBooksData(req, res) {
     const books = [];
-    const sqlValues = [];
+    const instruction1Values = [];
     const url = `https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.authorText}`;
     superagent
         .get(url)
         .then((data) => {
             // console.log('1111', data.body.items[0].volumeInfo);
             let narrowedData = data.body.items;
-            console.log('narrowedData is ', narrowedData[0].volumeInfo.industryIdentifiers[0].identifier);
+            console.log('narrowedData is ', narrowedData[0].volumeInfo.imageLinks);
             narrowedData.forEach((element) => {
                 // console.log(element.volumeInfo.imageLinks.thumbnail);
                 books.push(
                     new Book(
-                        element.volumeInfo.imageLinks.thumbnail || 'No Image Available',
+                        element.volumeInfo.imageLinks.thumbnail || undefined,
                         element.volumeInfo.title,
                         element.volumeInfo.authors,
                         element.volumeInfo.description,
-                        element.volumeInfo.industryIdentifiers[0].identifier || 'No ISBN Available'
+                        element.volumeInfo.industryIdentifiers[0].identifier
                     )
                 );
-                sqlValues.push([element.volumeInfo.imageLinks.thumbnail,
-                    element.volumeInfo.title,
-                    element.volumeInfo.authors,
-                    element.volumeInfo.description,
-                    element.volumeInfo.industryIdentifiers[0].identifier])
             });
-
-
             res.render('searches/show', { books: books });
         }).catch((err) => errorHandler(err, res));
 
     }
     
-app.post('/bookshelf', getBookshelf);
+app.post('/book', saveBooksToDatabase);
 
-function getBookshelf(req, res) {
-    const SQL = `INSERT INTO books(
+function saveBooksToDatabase(req, res) {
+    const instruction1 = `INSERT INTO books(
         image_url, title, author, descriptions, isbn, bookshelf
         ) VALUES (
           $1, $2, $3, $4, $5, $6
           )`;
-        client.query(SQL, [req.body.image, req.body.title, req.body.author, req.body.description, req.body.isbn, req.body.bookshelf]);
-	res.send(req.body);
-	// const sql = `SELECT * FROM `
-} 
+        client.query(instruction1, [req.body.image, req.body.title, req.body.author, req.body.description, req.body.isbn, req.body.bookshelf]);
+        
+    const instruction2 = `SELECT id FROM books WHERE title =$1`;
+    let value = [req.body.title];
+    
+    client.query(instruction2, value).then(singleBookDetail => {
+        console.log('3333333', singleBookDetail.rows[0].id);
+        let id = singleBookDetail.rows[singleBookDetail.rows.length-1].id;
+        res.redirect(`detail/${id}`);        
+    })       
+}
+
 app.get('/menu', displaySavedBooks)
 function displaySavedBooks (req, res) {
-    // res.render('index', {body: 'good day'})
-    client.query(`SELECT * FROM books;`).then(saveBooks =>{
-        console.log('saveBooks.rows isisisis', saveBooks.rows);
-        res.render('index', {books : saveBooks.rows});
+    client.query(`SELECT * FROM books;`).then(savedBooks =>{
+        console.log('savedBooks.rows isisisis', savedBooks.rows);
+        res.render('index', {books : savedBooks.rows});
     })
 }
 
 app.get('/detail/:id', showBookDetail);
 function showBookDetail(req, res){
-    // res.send('onde book');
     client.query(`SELECT * FROM books where id =$1`,[req.params.id]).then(singleBookData => {
         console.log('1111111',singleBookData.rows[0]);
         let book = singleBookData.rows[0]
